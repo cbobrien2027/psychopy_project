97f87c67a1e4d3d314082b6628ae79dfc813f564
cbobrien2027 <162505045+cbobrien2027@users.noreply.github.com>
2024-05-01 20:02:50 -0400

    trying to get code into builder

--- test2.py
+++ test2.py
@@ -11,19 +11,26 @@ instruction_msg_time = event.waitKeys (keyList = ['space'])
 xvalues_range = [0, 3, 4, 3, 0, -3, -4, -3]
 yvalues_range = [4, 3, 0, -3, -4, -3, 0, 3]
 
-possible_positions = [(0, 4), (3, 3), (4, 0), (3, -3), 
+possible_positions = [(0, 4), (3, 3), (4, 0), (3, -3), (0, -4), (-3, -3), (-4, 0), (-3, 3)]
 
-if
-for ii in range (8):
-    for jj in range (8):
-        if xvalues_range [ii] == xvalues_range [jj] and yvalues_range [ii] == yvalues_range [jj]:
-            triangle1 = visual.Polygon(win = mywin, edges=3, radius=1.25, fillColor=[0, 255, 0], lineColor = [0, 255, 0], colorSpace = 'rgb', pos=(xvalues_range [jj], yvalues_range [jj]))
-            triangle1.draw()
+for ii in possible_positions:
+    if ii == triangle_pos:
+        triangle1 = visual.Polygon(win = mywin, edges=3, radius=1.25, colorSpace = 'rgb', pos=ii)
+        if triangle_color == 'g':
+            triangle1.color = (0, 255, 0)
         else:
-            circle1 = visual.Circle (win = mywin, radius= 1, edges='circle', fillColor= [0, 255, 0], colorSpace = 'rgb', pos=(xvalues_range [jj], yvalues_range [jj]))
-            circle1.draw()
-    myphoto = mywin.update()
-    core.wait (1.0)
+            triangle1.color = (255, 0, 0)
+        triangle1.draw()
+    else:
+        circle1 = visual.Circle (win = mywin, radius= 1, edges='circle', colorSpace = 'rgb', pos=ii)
+        if ii == pink_circle_pos:
+            circle1.color (255, 0, 0)
+        else:
+            circle1.color (0, 255, 0)
+        circle1.draw()
+    mywin.update()
+        
+
     #while clicked == False:
         #if mouse.isPressedIn (triangle1):
             #clicked = True
@@ -35,33 +42,3 @@ for ii in range (8):
             #core.quit()
         #else:
             #core.wait (3)
-        
-    
-
-for ii in range (8):
-    for jj in range (8):
-        if xvalues_range [ii] == xvalues_range [jj] and yvalues_range [ii] == yvalues_range [jj]:
-            triangle1 = visual.Polygon(win = mywin, edges=3, radius=1.25, fillColor=[255, 0, 0], lineColor = [255, 0, 0], colorSpace = 'rgb', pos=(xvalues_range [jj], yvalues_range [jj]))
-            triangle1.draw()
-        else:
-            circle1 = visual.Circle (win = mywin, radius= 1, edges='circle', fillColor= [0, 255, 0], colorSpace = 'rgb', pos=(xvalues_range [jj], yvalues_range [jj]))
-            circle1.draw()
-    mywin.update()
-    core.wait (1.0)
-    
-scrambled_x_range = [-3, 0, 0, 4, -3, -4, 3, 3]
-scrambled_y_range = [-3, -4, 4, 0, 3, 0, -3, 3]
-
-for ii in range (8):
-    for jj in range (8):
-        if xvalues_range [ii] == xvalues_range [jj] and yvalues_range [ii] == yvalues_range [jj]:
-            triangle1 = visual.Polygon(win = mywin, edges=3, radius=1.25, fillColor=[0, 255, 0], lineColor = [0, 255, 0], colorSpace = 'rgb', pos=(xvalues_range [jj], yvalues_range [jj]))
-            triangle1.draw()
-        elif xvalues_range [jj] == scrambled_x_range [ii] and yvalues_range [jj] == scrambled_y_range [ii]:
-            circle2 = visual.Circle (win = mywin, radius= 1, edges='circle', fillColor= [255, 0, 0], colorSpace = 'rgb', pos=(xvalues_range [jj], yvalues_range [jj]))
-            circle2.draw()
-        else:
-            circle1 = visual.Circle (win = mywin, radius= 1, edges='circle', fillColor= [0, 255, 0], colorSpace = 'rgb', pos=(xvalues_range [jj], yvalues_range [jj]))
-            circle1.draw()
-    mywin.update()
-    core.wait (1.0)
